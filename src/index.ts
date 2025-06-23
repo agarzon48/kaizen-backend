fcd585b61a50304ffe230dbfb50a8a7f0de330e5
Adrián Garzón <garzonximenez@gmail.com>
2025-06-23 09:40:58 +0200

    FEAT: Rename infrastructure folder

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './infraestructure/express/routes';
+import routes from './infrastructure/express/routes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
